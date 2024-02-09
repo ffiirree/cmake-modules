4fb7172e66da7756e231c0f9bd161d71c7b69d49
ffiirree <ice_qi@163.com>
2024-02-10 06:46:21 +0800

    feat(FFmpeg): IMPLIB & LIBRARY

--- FindFFmpeg.cmake
+++ FindFFmpeg.cmake
@@ -1,165 +1,138 @@
-#
 # This module defines the following variables:
 #
-# FFMPEG_FOUND          - All required components and the core library were found
-# FFMPEG_INCLUDE_DIRS   - Combined list of all components include dirs
-# FFMPEG_LIBRARIES      - Combined list of all components libraries
-# FFMPEG_VERSION        - Version defined in libavutil/ffversion.h
+# FFmpeg_FOUND          - All required components and the core library were found
+# FFmpeg_INCLUDE_DIRS   - Combined list of all components include dirs
+# FFmpeg_LIBRARIES      - Combined list of all components libraries
+# FFmpeg_VERSION        - Version defined in libavutil/ffversion.h
 #
 # ffmpeg::ffmpeg        - FFmpeg target
 #
 # For each requested component the following variables are defined:
 #
-# FFMPEG_<component>_FOUND          - The component was found
-# FFMPEG_<component>_INCLUDE_DIRS   - The components include dirs
-# FFMPEG_<component>_LIBRARIES      - The components libraries
-# FFMPEG_<component>_VERSION        - The components version
-# FFMPEG_<component>_VERSION_MAJOR  - The components major version
-# FFMPEG_<component>_VERSION_MINOR  - The components minor version
-# FFMPEG_<component>_VERSION_MICRO  - The components micro version
+# FFmpeg_<component>_FOUND          - The component was found
+# FFmpeg_<component>_INCLUDE_DIRS   - The components include dirs
+# FFmpeg_<component>_LIBRARIES      - The components libraries
+# FFmpeg_<component>_VERSION        - The components version
 #
 # ffmpeg::<component>               - The component target
 #
-# <component> is the uppercase name of the component
-# 
 # Usage:
 #   find_package(FFmpeg REQUIRED)
 #   find_package(FFmpeg 5.1.2 COMPONENTS avutil avcodec avformat avdevice avfilter REQUIRED)
 
-
 find_package(PkgConfig QUIET)
 
 # find the root dir for specified version
-function(find_ffmpeg_root_by_version version)
+function(find_ffmpeg_root_by_version EXPECTED_VERSION)
     set(FFMPEG_FIND_PATHS $ENV{PATH} $ENV{FFMPEG_PATH} $ENV{FFMPEG_ROOT} /opt /usr /sw)
 
-    set(_found_version)
-    set(_found_root_dir)
+    set(FOUND_VERSION)
+    set(FOUND_ROOT_DIR)
 
-    set(INDEX 1)
-
-    foreach(path ${FFMPEG_FIND_PATHS})
-        unset(FFMPEG_${INDEX}_HEADER_DIR CACHE)
-        find_path(
-            FFMPEG_${INDEX}_HEADER_DIR
-            NAMES "libavutil/ffversion.h"
-            PATHS ${path} ${path}/include/${CMAKE_LIBRARY_ARCHITECTURE}
-            PATH_SUFFIXES ffmpeg include
+    foreach(ROOT_DIR ${FFMPEG_FIND_PATHS})
+        unset(FFMPEG_VERSION_HEADER CACHE)
+        find_file(
+            FFMPEG_VERSION_HEADER
+            NAMES "ffversion.h"
+            PATHS ${ROOT_DIR} ${ROOT_DIR}/include ${ROOT_DIR}/include/${CMAKE_LIBRARY_ARCHITECTURE}
+            PATH_SUFFIXES libavutil
             NO_DEFAULT_PATH
         )
-        mark_as_advanced(FFMPEG_${INDEX}_HEADER_DIR)
+        mark_as_advanced(FFMPEG_VERSION_HEADER)
 
-        if(NOT "${FFMPEG_${INDEX}_HEADER_DIR}" STREQUAL "FFMPEG_${INDEX}_HEADER_DIR-NOTFOUND")
-            # parse ffmpeg version
-            set(_ffmpeg_version_header "${FFMPEG_${INDEX}_HEADER_DIR}/libavutil/ffversion.h")
+        if(NOT "${FFMPEG_VERSION_HEADER}" STREQUAL "FFMPEG_VERSION_HEADER-NOTFOUND")
+            file(STRINGS "${FFMPEG_VERSION_HEADER}" FFMPEG_VERSION_STRING REGEX "FFMPEG_VERSION")
 
-            if(EXISTS "${_ffmpeg_version_header}")
-                file(STRINGS "${_ffmpeg_version_header}" __ffmpeg_version_line REGEX "FFMPEG_VERSION")
+            # #define FFMPEG_VERSION "6.0-full_build-www.gyan.dev"
+            # fixme:    #define FFMPEG_VERSION "N-111059-gd78bffbf3d"
+            string(REGEX REPLACE ".*FFMPEG_VERSION[ \t]+\"[n]?([0-9\\.]*).*\"" "\\1" CURRENT_VERSION "${FFMPEG_VERSION_STRING}")
 
-                #           #define FFMPEG_VERSION "6.0-full_build-www.gyan.dev"
-                # fixme:    #define FFMPEG_VERSION "N-111059-gd78bffbf3d"
-                string(REGEX REPLACE ".*FFMPEG_VERSION[ \t]+\"[n]?([0-9\\.]*).*\"" "\\1" _ffmpeg_version "${__ffmpeg_version_line}")
-
-                # if do not specified, return the fist one
-                if("${version}" STREQUAL "")
-                    set(_found_version ${_ffmpeg_version})
-                    set(_found_root_dir ${path})
-                    break()
-                elseif(${_ffmpeg_version} VERSION_GREATER_EQUAL "${version}") # otherwise, the minimum one of suitable versions
-                    if((NOT _found_version) OR(${_ffmpeg_version} VERSION_LESS "${_found_version}"))
-                        set(_found_version ${_ffmpeg_version})
-                        set(_found_root_dir ${path})
-                    endif()
-                endif()
-
-            else()
-                message(FATAL_ERROR "Can not find the version file: 'libavutil/ffversion.h'.")
+            # not specified, return the first one
+            if("${EXPECTED_VERSION}" STREQUAL "")
+                set(FOUND_VERSION ${CURRENT_VERSION})
+                set(FOUND_ROOT_DIR ${ROOT_DIR})
+                break()
             endif()
 
-            unset(_ffmpeg_version_header)
-
-            math(EXPR INDEX ${INDEX}+1)
+            # otherwise, the minimum one of suitable versions
+            if(${CURRENT_VERSION} VERSION_GREATER_EQUAL "${EXPECTED_VERSION}")
+                if((NOT FOUND_VERSION) OR(${CURRENT_VERSION} VERSION_LESS "${FOUND_VERSION}"))
+                    set(FOUND_VERSION ${CURRENT_VERSION})
+                    set(FOUND_ROOT_DIR ${ROOT_DIR})
+                endif()
+            endif()
         endif()
     endforeach()
 
-    set(FFMPEG_VERSION ${_found_version} PARENT_SCOPE)
-    set(FFMPEG_ROOT_DIR ${_found_root_dir} PARENT_SCOPE)
+    set(FFmpeg_VERSION ${FOUND_VERSION} PARENT_SCOPE)
+    set(FFmpeg_ROOT_DIR ${FOUND_ROOT_DIR} PARENT_SCOPE)
 endfunction()
 
 # find a ffmpeg component
-function(find_ffmpeg_library root_dir component header)
-    string(TOUPPER "${component}" component_u)
-
-    set(FFMPEG_${component_u}_FOUND FALSE PARENT_SCOPE)
-    set(FFmpeg_${component}_FOUND FALSE PARENT_SCOPE)
-
-    unset(FFMPEG_${component}_INCLUDE_DIR CACHE)
-    unset(FFMPEG_${component}_LIBRARY CACHE)
-
+function(find_ffmpeg_component ROOT_DIR COMPONENT HEADER)
+    # header
     find_path(
-        FFMPEG_${component}_INCLUDE_DIR
-        NAMES "lib${component}/${header}" "lib${component}/version.h"
-        PATHS ${root_dir} ${root_dir}/include/${CMAKE_LIBRARY_ARCHITECTURE}
+        FFmpeg_${COMPONENT}_INCLUDE_DIR
+        NAMES "lib${COMPONENT}/${HEADER}" "lib${COMPONENT}/version.h"
+        PATHS ${ROOT_DIR} ${ROOT_DIR}/include/${CMAKE_LIBRARY_ARCHITECTURE}
         PATH_SUFFIXES ffmpeg libav include
         NO_DEFAULT_PATH
     )
 
-    find_library(
-        FFMPEG_${component}_LIBRARY
-        NAMES "${component}" "lib${component}"
-        PATHS ${root_dir} ${root_dir}/lib/${CMAKE_LIBRARY_ARCHITECTURE}
-        PATH_SUFFIXES lib lib64 bin bin64
-        NO_DEFAULT_PATH
-    )
-
-    set(FFMPEG_${component_u}_INCLUDE_DIRS ${FFMPEG_${component}_INCLUDE_DIR} PARENT_SCOPE)
-    set(FFMPEG_${component_u}_LIBRARIES ${FFMPEG_${component}_LIBRARY} PARENT_SCOPE)
-
-    mark_as_advanced(FFMPEG_${component}_INCLUDE_DIR FFMPEG_${component}_LIBRARY)
-
-    if(FFMPEG_${component}_INCLUDE_DIR AND FFMPEG_${component}_LIBRARY)
-        set(FFMPEG_${component_u}_FOUND TRUE PARENT_SCOPE)
-        set(FFmpeg_${component}_FOUND TRUE PARENT_SCOPE)
-
-        # headers
-        list(APPEND FFMPEG_INCLUDE_DIRS ${FFMPEG_${component}_INCLUDE_DIR})
-        list(REMOVE_DUPLICATES FFMPEG_INCLUDE_DIRS)
-        set(FFMPEG_INCLUDE_DIRS "${FFMPEG_INCLUDE_DIRS}" PARENT_SCOPE)
-
-        # libs
-        list(APPEND FFMPEG_LIBRARIES ${FFMPEG_${component}_LIBRARY})
-        list(REMOVE_DUPLICATES FFMPEG_LIBRARIES)
-        set(FFMPEG_LIBRARIES "${FFMPEG_LIBRARIES}" PARENT_SCOPE)
-
-        # version
-        set(FFMPEG_${component_u}_VERSION "unknown" PARENT_SCOPE)
-
-        set(_major_version_header "${FFMPEG_${component}_INCLUDE_DIR}/lib${component}/version_major.h")
-        set(_version_header "${FFMPEG_${component}_INCLUDE_DIR}/lib${component}/version.h")
-
-        if(EXISTS "${_version_header}")
-            # major version
-            if(EXISTS ${_major_version_header})
-                file(STRINGS "${_major_version_header}" _major_version REGEX "^.*VERSION_MAJOR[ \t]+[0-9]+[ \t]*$")
-            endif()
-
-            # other
-            file(STRINGS "${_version_header}" _version REGEX "^.*VERSION_(MAJOR|MINOR|MICRO)[ \t]+[0-9]+[ \t]*$")
-
-            list(APPEND _version ${_major_version})
-
-            string(REGEX REPLACE ".*VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" _major "${_version}")
-            string(REGEX REPLACE ".*VERSION_MINOR[ \t]+([0-9]+).*" "\\1" _minor "${_version}")
-            string(REGEX REPLACE ".*VERSION_MICRO[ \t]+([0-9]+).*" "\\1" _micro "${_version}")
-
-            set(FFMPEG_${component_u}_VERSION_MAJOR "${_major}" PARENT_SCOPE)
-            set(FFMPEG_${component_u}_VERSION_MINOR "${_minor}" PARENT_SCOPE)
-            set(FFMPEG_${component_u}_VERSION_MICRO "${_micro}" PARENT_SCOPE)
-
-            set(FFMPEG_${component_u}_VERSION "${_major}.${_minor}.${_micro}" PARENT_SCOPE)
-        else()
-            message(STATUS "Failed parsing FFmpeg ${component} version")
+    # version
+    if(EXISTS "${FFmpeg_${COMPONENT}_INCLUDE_DIR}/lib${COMPONENT}/version.h")
+        if(EXISTS "${FFmpeg_${COMPONENT}_INCLUDE_DIR}/lib${COMPONENT}/version_major.h")
+            file(STRINGS "${FFmpeg_${COMPONENT}_INCLUDE_DIR}/lib${COMPONENT}/version_major.h" MAJOR_VERSION_STRING REGEX "^.*VERSION_MAJOR[ \t]+[0-9]+[ \t]*$")
         endif()
+
+        # other
+        file(STRINGS "${FFmpeg_${COMPONENT}_INCLUDE_DIR}/lib${COMPONENT}/version.h" VERSION_STRING REGEX "^.*VERSION_(MAJOR|MINOR|MICRO)[ \t]+[0-9]+[ \t]*$")
+
+        list(APPEND VERSION_STRING ${MAJOR_VERSION_STRING})
+
+        string(REGEX REPLACE ".*VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" MAJOR "${VERSION_STRING}")
+        string(REGEX REPLACE ".*VERSION_MINOR[ \t]+([0-9]+).*" "\\1" MINOR "${VERSION_STRING}")
+        string(REGEX REPLACE ".*VERSION_MICRO[ \t]+([0-9]+).*" "\\1" PATCH "${VERSION_STRING}")
+
+        set(FFmpeg_${COMPONENT}_VERSION "${MAJOR}.${MINOR}.${PATCH}" PARENT_SCOPE)
+    else()
+        message(STATUS "'${FFmpeg_${COMPONENT}_INCLUDE_DIR}/lib${COMPONENT}/version.h' does not exist.")
+    endif()
+
+    # library
+    if(WIN32)
+        find_library(
+            FFmpeg_${COMPONENT}_IMPLIB
+            NAMES "${COMPONENT}" "lib${COMPONENT}"
+            PATHS ${ROOT_DIR} ${ROOT_DIR}/lib/${CMAKE_LIBRARY_ARCHITECTURE}
+            PATH_SUFFIXES lib lib64 bin bin64
+            NO_DEFAULT_PATH
+        )
+
+        find_program(
+            FFmpeg_${COMPONENT}_LIBRARY
+            NAMES "${COMPONENT}-${MAJOR}.dll" "${COMPONENT}.dll"
+            PATHS ${ROOT_DIR} ${ROOT_DIR}/bin
+            NO_DEFAULT_PATH
+        )
+    else()
+        find_library(
+            FFmpeg_${COMPONENT}_LIBRARY
+            NAMES "${COMPONENT}" "lib${COMPONENT}"
+            PATHS ${ROOT_DIR} ${ROOT_DIR}/lib/${CMAKE_LIBRARY_ARCHITECTURE}
+            PATH_SUFFIXES lib lib64 bin bin64
+            NO_DEFAULT_PATH
+        )
+    endif()
+
+    mark_as_advanced(FFmpeg_${COMPONENT}_INCLUDE_DIR FFmpeg_${COMPONENT}_LIBRARY FFmpeg_${COMPONENT}_IMPLIB)
+
+    if(FFmpeg_${COMPONENT}_INCLUDE_DIR AND FFmpeg_${COMPONENT}_LIBRARY)
+        set(FFmpeg_${COMPONENT}_FOUND TRUE PARENT_SCOPE)
+
+        set(FFmpeg_${COMPONENT}_IMPLIBS ${FFmpeg_${COMPONENT}_IMPLIB} PARENT_SCOPE)
+        set(FFmpeg_${COMPONENT}_LIBRARIES ${FFmpeg_${COMPONENT}_LIBRARY} PARENT_SCOPE)
+        set(FFmpeg_${COMPONENT}_INCLUDE_DIRS ${FFmpeg_${COMPONENT}_INCLUDE_DIR} PARENT_SCOPE)
     endif()
 endfunction()
 
@@ -168,76 +141,76 @@ if(NOT FFmpeg_FIND_COMPONENTS)
     list(APPEND FFmpeg_FIND_COMPONENTS avutil avcodec avdevice avfilter avformat swresample swscale postproc)
 endif()
 
-set(FFMPEG_VERSION)
 find_ffmpeg_root_by_version("${FFmpeg_FIND_VERSION}")
 
-if((NOT FFMPEG_VERSION) OR(NOT FFMPEG_ROOT_DIR))
+if((NOT FFmpeg_VERSION) OR(NOT FFmpeg_ROOT_DIR))
     message(FATAL_ERROR "Can not find the suitable version.")
 endif()
 
-set(FFMPEG_INCLUDE_DIRS)
-set(FFMPEG_LIBRARIES)
-
 list(REMOVE_DUPLICATES FFmpeg_FIND_COMPONENTS)
 
-foreach(component ${FFmpeg_FIND_COMPONENTS})
-    if(component STREQUAL "avcodec")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avcodec.h")
-    elseif(component STREQUAL "avdevice")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avdevice.h")
-    elseif(component STREQUAL "avfilter")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avfilter.h")
-    elseif(component STREQUAL "avformat")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avformat.h")
-    elseif(component STREQUAL "avresample")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avresample.h")
-    elseif(component STREQUAL "avutil")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "avutil.h")
-    elseif(component STREQUAL "postproc")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "postprocess.h")
-    elseif(component STREQUAL "swresample")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "swresample.h")
-    elseif(component STREQUAL "swscale")
-        find_ffmpeg_library(${FFMPEG_ROOT_DIR} "${component}" "swscale.h")
+foreach(COMPONENT ${FFmpeg_FIND_COMPONENTS})
+    if(COMPONENT STREQUAL "postproc")
+        find_ffmpeg_component(${FFmpeg_ROOT_DIR} ${COMPONENT} "postprocess.h")
     else()
-        message(FATAL_ERROR "Unknown FFmpeg component requested: ${component}")
+        find_ffmpeg_component(${FFmpeg_ROOT_DIR} ${COMPONENT} "${component}.h")
+    endif()
+
+    if(FFmpeg_${COMPONENT}_FOUND)
+        list(APPEND FFmpeg_LIBRARIES ${FFmpeg_${COMPONENT}_LIBRARIES})
+        list(APPEND FFmpeg_INCLUDE_DIRS ${FFmpeg_${COMPONENT}_INCLUDE_DIRS})
     endif()
 endforeach()
 
+list(REMOVE_DUPLICATES FFmpeg_LIBRARIES)
+list(REMOVE_DUPLICATES FFmpeg_INCLUDE_DIRS)
+
+#
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(
     FFmpeg
-    FOUND_VAR FFMPEG_FOUND
-    REQUIRED_VARS FFMPEG_INCLUDE_DIRS FFMPEG_LIBRARIES
-    VERSION_VAR FFMPEG_VERSION
+    FOUND_VAR FFmpeg_FOUND
+    REQUIRED_VARS FFmpeg_ROOT_DIR FFmpeg_INCLUDE_DIRS FFmpeg_LIBRARIES
+    VERSION_VAR FFmpeg_VERSION
     HANDLE_COMPONENTS
 )
 
-if(FFMPEG_FOUND)
-    foreach(component ${FFmpeg_FIND_COMPONENTS})
-        if(NOT TARGET ffmpeg::${component})
-            string(TOUPPER ${component} component_u)
+if(FFmpeg_FOUND)
+    if(NOT TARGET ffmpeg::ffmpeg)
+        add_library(ffmpeg::ffmpeg INTERFACE IMPORTED)
+    endif()
 
-            if(FFMPEG_${component_u}_FOUND)
-                if(IS_ABSOLUTE "${FFMPEG_${component_u}_LIBRARIES}")
-                    add_library(ffmpeg::${component} UNKNOWN IMPORTED)
-                    set_target_properties(ffmpeg::${component} PROPERTIES IMPORTED_LOCATION "${FFMPEG_${component_u}_LIBRARIES}")
+    foreach(component IN LISTS FFmpeg_FIND_COMPONENTS)
+        if(FFmpeg_${component}_FOUND AND NOT TARGET ffmpeg::${component})
+            if(IS_ABSOLUTE "${FFmpeg_${component}_LIBRARIES}")
+                if(DEFINED FFmpeg_${component}_IMPLIBS)
+                    if(FFmpeg_${component}_IMPLIBS STREQUAL FFmpeg_${component}_LIBRARIES)
+                        add_library(ffmpeg::${component} STATIC IMPORTED)
+                    else()
+                        add_library(ffmpeg::${component} SHARED IMPORTED)
+                        set_property(TARGET ffmpeg::${component} PROPERTY IMPORTED_IMPLIB "${FFmpeg_${component}_IMPLIBS}")
+                    endif()
                 else()
-                    add_library(ffmpeg::${component} INTERFACE IMPORTED)
-                    set_target_properties(ffmpeg::${component} PROPERTIES IMPORTED_LIBNAME "${FFMPEG_${component_u}_LIBRARIES}")
+                    add_library(ffmpeg::${component} UNKNOWN IMPORTED)
                 endif()
 
-                set_target_properties(ffmpeg::${component} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${FFMPEG_${component_u}_INCLUDE_DIRS}")
+                set_property(TARGET ffmpeg::${component} PROPERTY IMPORTED_LOCATION "${FFmpeg_${component}_LIBRARIES}")
+            else()
+                add_library(ffmpeg::${component} INTERFACE IMPORTED)
+                set_target_properties(ffmpeg::${component} PROPERTIES IMPORTED_LIBNAME "${FFmpeg_${component}_LIBRARIES}")
+            endif()
+
+            set_target_properties(ffmpeg::${component} PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${FFmpeg_${component}_INCLUDE_DIRS}"
+                VERSION "${FFmpeg_${component}_VERSION}"
+            )
+
+            get_target_property(FFMPEG_INTERFACE_LIBRARIES ffmpeg::ffmpeg INTERFACE_LINK_LIBRARIES)
+
+            if(NOT ffmpeg::${component} IN_LIST FFMPEG_INTERFACE_LIBRARIES)
+                set_property(TARGET ffmpeg::ffmpeg APPEND PROPERTY INTERFACE_LINK_LIBRARIES ffmpeg::${component})
             endif()
         endif()
     endforeach()
-
-    if(NOT TARGET ffmpeg::ffmpeg)
-        if(FFMPEG_FOUND)
-            add_library(ffmpeg::ffmpeg INTERFACE IMPORTED)
-            set_property(TARGET ffmpeg::ffmpeg PROPERTY INTERFACE_LINK_LIBRARIES ${FFMPEG_LIBRARIES})
-            set_property(TARGET ffmpeg::ffmpeg PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIRS})
-        endif()
-    endif()
 endif()
